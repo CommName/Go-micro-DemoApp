12e0a956322ec8d945e3e47be2b3d6310c279332
CommName <17132081+CommName@users.noreply.github.com>
2020-04-13 17:26:15 +0200

    Env simulator, temperature changes dynamically

--- TemperatureControlSystem/ThermometarMain.go
+++ TemperatureControlSystem/ThermometarMain.go
@@ -11,7 +11,18 @@ import (
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
+
+func EnviermentSimulator(termometar *handler.Thermometar) error {
+	for ; true ; {
+		temperatureChange := int64(rand.Uint64()%20) -10
+		termometar.Temperature += temperatureChange
+		time.Sleep(1000 * time.Millisecond)
+	}
+	return nil
+}
 
 func main() {
 	var roomName string
@@ -22,7 +33,7 @@ func main() {
 	// New Service
 	service := micro.NewService(
 		micro.Name("iots.temperature.srv.Thermometar."+roomName),
-		micro.Version("v1.0"),
+		micro.Version("v1.1"),
 	)
 
 	// Initialise service
@@ -39,6 +50,9 @@ func main() {
 
 	// Register Function as Subscriber
 	micro.RegisterSubscriber("iots.temperature.srv.Thermometar."+roomName, service.Server(), subscriber.Handler)
+
+	//Env Simulator
+	go EnviermentSimulator(termometar)
 
 	// Run service
 	if err := service.Run(); err != nil {
